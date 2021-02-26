0e2094f891caf72a52637784c864d7235d18becd
bux <github@jonathandavid.de>
2021-02-26 15:37:26 +0100

    add typescript and vue parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
+    root: true,
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+    },
     extends: [
-        "plugin:vue/essential",
+        "plugin:vue/strongly-recommended",
         "eslint:recommended",
         "@vue/typescript/recommended",
         "prettier",
     ],
-    plugins: ["prettier"],
-
+    plugins: ["@typescript-eslint", "prettier"],
     rules: {
         "prettier/prettier": "error",
     },
